1b1c055388fe7ce772a9bee5888a80203a73adce
Soyolz <fritzfils7aime@gmail.com>
2025-01-22 23:59:39 -0500

    Changed Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import NavBar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Currently from "./components/Currently";
 import Projects from "./components/Projects";
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main>
-      <NavBar />
+      <Navbar />
       <About />
       <Currently />
       <Projects />
