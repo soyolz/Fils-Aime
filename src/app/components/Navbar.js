97d3d765f086d17a145caa4a5adac1cef94c6bfd
SOYOLZ <fritzfils7aime@gmail.com>
2025-12-12 12:06:55 -0500

    Resume Color

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -177,7 +177,7 @@ export default function Navbar() {
                                             href="./resume.pdf" 
                                             target="_blank"
                                             rel="noopener noreferrer"
-                                            className="btn flex items-center justify-center p-3 rounded-lg bg-gray-700 hover:bg-gray-600 transition-colors"
+                                            className="btn flex items-center justify-center p-3 rounded-lg bg-green-800 hover:bg-green-800/50 transition-colors"
                                         >
                                             <svg className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
